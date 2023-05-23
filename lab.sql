2969385dc9c0a2f161c2d81c0f756151cf78f33d
Tanmoy Choudhury <official.tanmoychoudhury@protonmail.com>
2023-05-23 17:12:25 +0530

    assignment 2

--- lab.sql
+++ lab.sql
@@ -45,3 +45,41 @@ alter table movie add price number(8,2);
 desc cust;
 desc movie;
 desc invoice;
+
+-- 2.1
+insert into cust values('a01', 'Bayross', 'Ivan', 'sa', 6125467);
+insert into cust values('a02', 'Saitwal', 'Vandana', 'mu', 5560379);
+insert into cust values('a03', 'Jaguste', 'Pramada', 'da', 4563891);
+insert into cust values('a04', 'Navindgi', 'Basu', 'ba', 6125401);
+insert into cust values('a05', 'Sreedharan', 'Ravi', 'va', null);
+insert into cust values('a06', '-', 'Rukmini', 'gh', 5125274);
+
+-- 2.2
+insert into movie values(1, 'bloody vengence', 'acion', 'jakie chan', 180.95);
+insert into movie values(2, 'the firm', 'thriller', 'tom cruise', 200.00);
+insert into movie values(3, 'pretty woman', 'romance', 'richard gere', 150.55);
+insert into movie values(4, 'home alone', 'comedy', 'macaulay culkin', 140.00);
+insert into movie values(5, 'the fugiive', 'thriller', 'harisson ford', 200.00);
+insert into movie values(6, 'coma', 'suspense', 'michael douglas', 100.00);
+insert into movie values(7, 'dracula', 'horror', 'gary oldman', 150.25);
+insert into movie values(8, 'quick change', 'comedy', 'bill muray', 100.00);
+insert into movie values(9, 'gone wih the wind', 'drama', 'clarke gable', 200.00);
+insert into movie values(10, 'carry on doctor', 'comedy', 'leslie phillips', 100.00);
+
+-- 2.3
+insert into invoice values('i01', 4, 'a01', '23-jul-93', '25-jul-93');
+insert into invoice values('i02', 3, 'a02', '12-aug-93', '15-aug-93');
+insert into invoice values('i03', 1, 'a02', '15-aug-93', '18-aug-93');
+insert into invoice values('i04', 6, 'a03', '10-sep-93', '12-sep-93');
+insert into invoice values('i05', 7, 'a04', '05-aug-93', '08-aug-93');
+insert into invoice values('i06', 2, 'a06', '18-sep-93', '21-sep-93');
+insert into invoice values('i07', 9, 'a05', '07-jul-93', '10-jul-93');
+insert into invoice values('i08', 9, 'a01', '11-aug-93', '14-aug-93');
+insert into invoice values('i09', 5, 'a03', '06-jul-93', '07-jul-93');
+insert into invoice values('i10', 8, 'a06', '03-sep-93', '06-sep-93');
+
+-- Display all 3 tables
+select * from cust;
+select * from movie;
+select * from invoice;
+
